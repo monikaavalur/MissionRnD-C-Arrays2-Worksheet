21e21eba31fd3e84abff8a67cb7e4a986ab2a7b8
monikaavalur <monikaavalur@missionrnd.com>
2016-01-12 20:03:24 +0530

    Submit Commit

--- src/countGreaterNumbers.cpp
+++ src/countGreaterNumbers.cpp
@@ -13,13 +13,48 @@ ERROR CASES: Return NULL for invalid inputs.
 
 NOTES:
 */
-
+#include<stdlib.h>
+int convert_int(char *);
 struct transaction {
 	int amount;
 	char date[11];
 	char description[20];
 };
 
-int countGreaterNumbers(struct transaction *Arr, int len, char *date) {
-	return -1;
+int countGreaterNumbers(struct transaction *Arr, int len, char *date)
+{
+	int count = 0;
+	 
+	for (int i = 0; i < sizeof(Arr); i++)
+	{
+		int date1 = convert_int(Arr[i].date);
+		int date2 = convert_int(date);
+		if (date1 == date2)
+			count++;
+	}
+	if (count != sizeof(Arr))
+	{
+		count = 0;
+		for (int i = 0; i < sizeof(Arr); i++)
+		{
+			int date1 = convert_int(Arr[i].date);
+			int date2 = convert_int(date);
+			if (date1 >= date2)
+				count++;
+		}
+		return count;
+	}
+	else return 0;
+
 }
+	
+int convert_int(char *date) 
+{
+	int day = strtol(date, &date, 10);
+	int month = strtol(++date, &date, 10);
+	int year = strtol(++date, &date, 10);
+	return (year * 12 + month) * 31 + day;
+}
+
+
+
